8c1141be6976fe80faf9b7319b61fb62a0730d15
irfathmostofa <irfathmostofa1@gmail.com>
2025-08-15 22:07:40 +0600

    new update

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -8,9 +8,9 @@ import {
 } from "./auth.controller";
 
 export default async function authRoutes(fastify: FastifyInstance) {
-  fastify.post("/register-user", registerHandler);
+  fastify.post("/create-user", registerHandler);
   fastify.post("/login-user", loginHandler);
   fastify.post("/get-user", getUsersHandler);
-  fastify.post("/register-admin", registerAdmin);
+  fastify.post("/create-admin", registerAdmin);
   fastify.post("/login-admin", loginHandlerAdmin);
 }
